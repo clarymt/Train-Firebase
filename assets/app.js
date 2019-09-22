700a9c671d6d3e31178f342213381a98f3a2f714
clarymt <clarymt@me.com>
2019-09-22 19:27:22 -0400

    finished all js functionality now time to decorate

--- assets/app.js
+++ assets/app.js
@@ -6,3 +6,93 @@
 //Code this app to calculate when the next train will arrive; this should be relative to the current time.
 //Users from many different machines must be able to view same train times.
 //Styling and theme are completely up to you. Get Creative!
+
+var firebaseConfig = {
+    apiKey: "AIzaSyClwv42n7HlcPLCjzHYbdvQz_AgzrIfEaA",
+    authDomain: "train-scheduler-ec5a0.firebaseapp.com",
+    databaseURL: "https://train-scheduler-ec5a0.firebaseio.com",
+    projectId: "train-scheduler-ec5a0",
+    storageBucket: "train-scheduler-ec5a0.appspot.com",
+    messagingSenderId: "889383584707",
+    appId: "1:889383584707:web:297c1b4213c3a72c457376"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  var database = firebase.database();
+
+  $("#add-train-btn").on("click", function(event) {
+    event.preventDefault();
+
+    var trainName = $("#train-name-input").val().trim();
+    var trainDestination = $("#destination-input").val().trim();
+    var trainTime = $("#first-train-input").val().trim();
+    var trainFrequency = $("#frequency-input").val().trim();
+
+    var newTrain = {
+      name: trainName,
+      destination: trainDestination,
+      time: trainTime,
+      frequency: trainFrequency
+    };
+
+    database.ref().push(newTrain);
+
+    console.log(newTrain.name);
+    console.log(newTrain.destination);
+    console.log(newTrain.time);
+    console.log(newTrain.frequency);
+
+    $("#train-name-input").val("");
+    $("#destination-input").val("");
+    $("#first-train-input").val("");
+    $("#frequency-input").val("");
+  });
+
+  database.ref().on("child_added", function(childSnapshot) {
+    console.log(childSnapshot.val());
+
+    var trainName = childSnapshot.val().name;
+    var trainDestination = childSnapshot.val().destination;
+    var trainTime = childSnapshot.val().time;
+    var trainFrequency = childSnapshot.val().frequency;
+
+    console.log(trainName);
+    console.log(trainDestination);
+    console.log(trainTime);
+    console.log(trainFrequency);
+
+    var firstTimeConverted = moment(trainTime, "HH:mm").subtract(1, "years");
+    console.log(firstTimeConverted);
+
+    // Current Time
+    var currentTime = moment();
+    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+    // Difference between the times
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    console.log("DIFFERENCE IN TIME: " + diffTime);
+
+    // Time apart (remainder)
+    var tRemainder = diffTime % trainFrequency;
+    console.log(tRemainder);
+
+    // Minute Until Train
+    var tMinutesTillTrain = trainFrequency - tRemainder;
+    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+    // Next Train
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+
+    var newRow = $("<tr>").append(
+      $("<td>").text(trainName),
+      $("<td>").text(trainDestination),
+      $("<td>").text(trainFrequency),
+      $("<td>").text(nextTrain.format("hh:mm")),
+      $("<td>").text(tMinutesTillTrain)
+    );
+
+    $("#train-table > tbody").append(newRow);
+
+  });
